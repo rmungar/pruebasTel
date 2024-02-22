f63b3a47417d5e9818e58d62ce2b0f891b48efe5
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2024-02-22 16:09:40 +0100

    RamaPaco 4

--- src/Telefono.kt
+++ src/Telefono.kt
@@ -3,6 +3,9 @@ class Telefono(val nombre: String, val marca: String) {
         require(nombre.isNotBlank())
         require(marca.isNotBlank())
     }
+    fun Contestar():String{
+        return "Hola , que tal ?"
+    }
 
     fun Colgar():String{
         return "Chao pescao"
