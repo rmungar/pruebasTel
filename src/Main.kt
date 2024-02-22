10db9c37588841f735c0929b51ab813a57c19a0d
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2024-02-22 16:11:18 +0100

    RamaPaco 5

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    println("Ven y sana mi dolor")
-    println("Tienes la cura de este amoooor")
-    println("Tu tienes la receta , la formula secreta")
+    val telefono2 = Telefono("Pepe" , "Iphone")
+    println(telefono2.Contestar())
+    println(telefono2.Colgar())
 }
