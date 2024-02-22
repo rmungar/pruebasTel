3b391c2f1023c7871c9c2a6141f4e4bb38e52148
rmungar <mgraul@iesrafaelalberti.es>
2024-02-22 15:49:32 +0100

    Psuh

--- src/Telefono.kt
+++ src/Telefono.kt
@@ -1,6 +1,11 @@
 class Telefono(val nombre: String, val marca: String) {
+    var estado: Boolean = false
     init {
         require(nombre.isNotBlank())
         require(marca.isNotBlank())
+    }
+    fun encender(){
+        estado = true
+        println("ENCENDIENDO....")
     }
 }
